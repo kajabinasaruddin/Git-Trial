d771a89d491476282b4b8245b2e1413077cd5d4c
kajabinasaruddin <kajabishaik624@gmail.com>
2025-07-17 15:56:16 +0530

    changed

---
+++ Khajabi.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Khajabi {
+    public static void main(String[] args) {
+        System.out.println("alice");
+        System.out.println("bob");
+        System.out.println("charliee");
+        System.out.println("danger");
+        System.out.println("eegar");
+    }
+}
